99752058c5a0a6b85587a6ff8333600414e5f0b1
KrishanKantKashyap <krishankantkashyap67123@gmail.com>
2025-03-24 10:14:03 +0530

    Update kk.py

--- group/kk.py
+++ group/kk.py
@@ -5,8 +5,3 @@ class Multiplier:
     def multiply_textual(self, a, b):
         return a * b
 
-if __name__ == "__main__":
-    multiplier = Multiplier()
-    print(multiplier.multiply_integers(5, 3))
-    print(multiplier.multiply_textual("abs", 2))
-
