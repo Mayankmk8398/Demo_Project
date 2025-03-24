c5de04a4af34d3bcf217630ebff4f6ad0608d755
KrishanKantKashyap <krishankantkashyap67123@gmail.com>
2025-03-24 10:00:23 +0530

    Create kk.py

---
+++ group/kk.py
@@ -0,0 +1,9 @@
+def multiply_integers(a, b):
+    return a * b
+
+def multiply_textual(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(multiply_integers(5, 3))  # Output: 15
+    print(multiply_textual("abs", 2))  # Output: absabs
