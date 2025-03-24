b0cd3885c1def80994dcb2922450ed69a147e557
KrishanKantKashyap <krishankantkashyap67123@gmail.com>
2025-03-24 10:04:46 +0530

    Update kk.py

--- group/kk.py
+++ group/kk.py
@@ -5,5 +5,5 @@ def multiply_textual(a, b):
     return a * b
 
 if __name__ == "__main__":
-    print(multiply_integers(5, 3))  # Output: 15
-    print(multiply_textual("abs", 2))  # Output: absabs
+    print(multiply_integers(5, 3))  
+    print(multiply_textual("abs", 2))
