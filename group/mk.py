62defda904703fc04696cb94d118c3c861b7ba07
Mayankmk8398 <mayankmalik8396@gmail.com>
2025-03-24 10:06:00 +0530

    Update mk.py

--- group/mk.py
+++ group/mk.py
@@ -1,9 +1,7 @@
+class Addition:
 def add(a, b):
-    """Return the sum of two integers."""
+
     return a + b
 
-def text_add(s1, s2):
-    """Return the concatenation of two strings."""
-    return s1 + s2
 
 
