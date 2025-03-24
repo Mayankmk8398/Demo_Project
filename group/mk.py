9c46b13b9d2c5d2633292de384fde5a43bf8cd67
Mayankmk8398 <mayankmalik8396@gmail.com>
2025-03-24 10:02:05 +0530

    Update mk.py

--- group/mk.py
+++ group/mk.py
@@ -6,9 +6,4 @@ def text_add(s1, s2):
     """Return the concatenation of two strings."""
     return s1 + s2
 
-if __name__ == "__main__":
-    # Testing integer addition
-    print("Integer Addition (5 + 3):", add(5, 3))
-    
-    # Testing textual addition
-    print("Textual Addition ('abs' + 'ent'):", text_add("abs", "ent"))
+
